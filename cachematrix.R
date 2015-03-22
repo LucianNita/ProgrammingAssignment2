447a60b3599473ed07b3d15039bf05e3c7dcd377
LucianNita <lucian.nita@yahoo.com>
2015-03-22 15:27:39 +0200

    ProgrammingAssignment2
    My new code

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,11 @@
 ## [Put comments here that describe what your functions do]
+
 makeCacheMatrix <- function(x = matrix()) {
+  
+  ## This function works like a class, it creates a list 
+  ## with 4 member functions: set, get, setInv and getInv. 
+  ## It uses <<- assignment operator so that these local 
+  ## variables are not visible to the outside environment. 
   
   xinv <- NULL 
   set <- function(y) {
@@ -17,6 +23,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  ## m is assigned the value of an inversed matrix from object 'x'.
+  ## If m is NOT NULL, then then the inversion result already exists.
+  ## In that case it is retuned and used. If m IS NULL we calculate the
+  ## inverse and we return it.
   m <- x$getInv() 
   if(!is.null(m)) { 
     message("getting cached data")
